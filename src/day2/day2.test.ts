30d38e93202b28002ef86d41565304166c2c8c57
Dilip Thomas <dthomas84@gmail.com>
2024-12-04 06:26:04 +1100

    day 2 solutions finally done.

--- src/day2/day2.test.ts
+++ src/day2/day2.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { p2, part1, part2 } from "./day2";
+import { part2, part1 } from "./day2";
 
 describe("day 2", () => {
   it("should return correct value for part 1 test input", () => {
@@ -15,10 +15,6 @@ describe("day 2", () => {
   });
 
   it("should return correct value for part 2 real input", () => {
-    expect(part2("real-input.txt")).toEqual(1);
-  });
-
-  it("should do", () => {
-    p2("real-input.txt");
+    expect(part2("real-input.txt")).toEqual(465);
   });
 });
--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -8,25 +8,10 @@ export function part1(filename: string) {
     .split("\n")
     .map((line) => line.split(" ").map((ch) => parseInt(ch)));
 
-  return reports.reduce((safeCount, report) => {
-    if (report[0] === report[1]) {
-      return safeCount;
-    }
-
-    const isAsc = report[0] < report[1];
-    for (let i = 0; i < report.length - 1; i++) {
-      const currentPairIsAsc = report[i] < report[i + 1];
-      if ((isAsc && !currentPairIsAsc) || (!isAsc && currentPairIsAsc)) {
-        return safeCount;
-      }
-      const diff = Math.abs(report[i] - report[i + 1]);
-      if (diff < 1 || diff > 3) {
-        return safeCount;
-      }
-    }
-
-    return safeCount + 1;
-  }, 0);
+  return reports.reduce(
+    (safeCount, report) => (isReportSafe(report) ? safeCount + 1 : safeCount),
+    0
+  );
 }
 
 export function part2(filename: string) {
@@ -36,71 +21,23 @@ export function part2(filename: string) {
     .split("\n")
     .map((line) => line.split(" ").map((ch) => parseInt(ch)));
 
-  const unsafeReports: { [key: number]: number[][] } = {};
+  let quarantine: number[][] = [];
   let safeCount = 0;
 
-  for (let j = 0; j < reports.length; j++) {
-    let report = reports[j];
-    if (report[0] === report[1]) {
-      unsafeReports[j] = [report.slice(1)];
-      continue;
-    }
-
-    let safe = true;
-    const isAsc = report[0] < report[1];
-    for (let i = 0; i < report.length - 1; i++) {
-      const isCurrentPairAsc = report[i] < report[i + 1];
-      if ((isAsc && !isCurrentPairAsc) || (!isAsc && isCurrentPairAsc)) {
-        const unsafe1 = [...report];
-        const unsafe2 = [...report];
-        unsafe1.splice(i, 1);
-        unsafe2.splice(i + 1, 1);
-        unsafeReports[j] = [unsafe1, unsafe2];
-        safe = false;
-        break;
-      }
-      const diff = Math.abs(report[i] - report[i + 1]);
-      if (diff < 1 || diff > 3) {
-        const unsafe1 = [...report];
-        const unsafe2 = [...report];
-        unsafe1.splice(i, 1);
-        unsafe2.splice(i + 1, 1);
-        unsafeReports[j] = [unsafe1, unsafe2];
-        safe = false;
-        break;
-      }
-    }
-
-    if (safe) {
+  for (let i = 0; i < reports.length; i++) {
+    let report = reports[i];
+    if (isReportSafe(report)) {
       safeCount++;
+    } else {
+      quarantine.push(report);
     }
   }
 
-  for (const [key, unsafes] of Object.entries(unsafeReports)) {
-    for (const report of unsafes) {
-      if (report[0] === report[1]) {
-        continue;
-      }
-
-      let safe = true;
-      const isAsc = report[0] < report[1];
-      for (let i = 0; i < report.length - 1; i++) {
-        const currentPairIsAsc = report[i] < report[i + 1];
-        if ((isAsc && !currentPairIsAsc) || (!isAsc && currentPairIsAsc)) {
-          console.log(`${key} unsafe: ${JSON.stringify(report)}`);
-          safe = false;
-          break;
-        }
-        const diff = Math.abs(report[i] - report[i + 1]);
-        if (diff < 1 || diff > 3) {
-          console.log(`${key} unsafe: ${JSON.stringify(report)}`);
-          safe = false;
-          break;
-        }
-      }
-
-      if (safe) {
-        console.log(`${key} safe! ${JSON.stringify(report)}`);
+  for (let report of quarantine) {
+    for (let i = 0; i < report.length; i++) {
+      let copy = [...report];
+      copy.splice(i, 1);
+      if (isReportSafe(copy)) {
         safeCount++;
         break;
       }
@@ -110,71 +47,7 @@ export function part2(filename: string) {
   return safeCount;
 }
 
-export function p2(filename: string) {
-  const reports = fs
-    .readFileSync(path.join(__dirname, filename))
-    .toString()
-    .split("\n")
-    .map((line) => line.split(" ").map((ch) => parseInt(ch)));
-
-  let unsafeReports: { [key: number]: number[][] } = {};
-  let safeCount = 0;
-
-  for (let i = 0; i < reports.length; i++) {
-    let report = reports[i];
-    let safe = true;
-    const isAsc = report[0] < report[1];
-
-    for (let j = 0; j < report.length - 1; j++) {
-      if (!isSafe(report[j], report[j + 1], isAsc)) {
-        safe = false;
-        let unsafeLeft = [...report];
-        let unsafeRight = [...report];
-        unsafeLeft.splice(j, 1);
-        unsafeRight.splice(j + 1, 1);
-        unsafeReports[i] = [unsafeLeft, unsafeRight];
-        break;
-      }
-    }
-
-    if (safe) {
-      safeCount++;
-    }
-  }
-
-  console.log(safeCount);
-  console.log(Object.values(unsafeReports).flat().length);
-
-  for (let [key, [try1, try2]] of Object.entries(unsafeReports)) {
-    let safe = true;
-    for (let i = 0; i < try1.length - 1; i++) {
-      const isAsc = try1[0] < try1[1];
-      if (!isSafe(try1[i], try1[i+1], isAsc)) {
-        safe = false;
-        break;
-      }
-    }
-    if (safe) {
-      safeCount++;
-    } else {
-      safe = true;
-      for (let i = 0; i < try2.length - 1; i++) {
-        const isAsc = try2[0] < try2[1];
-        if (!isSafe(try2[i], try2[i + 1], isAsc)) {
-          safe = false;
-          break;
-        }
-      }
-      if (safe) {
-        safeCount++;
-      }
-    }
-  }
-
-  console.log(safeCount);
-}
-
-function isSafe(left: number, right: number, isAsc: boolean): boolean {
+function isPairSafe(left: number, right: number, isAsc: boolean): boolean {
   if (left === right) {
     return false;
   }
@@ -190,4 +63,16 @@ function isSafe(left: number, right: number, isAsc: boolean): boolean {
   }
 
   return true;
+}
+
+function isReportSafe(report: number[]) {
+  let safe = true;
+  const isAsc = report[0] < report[1];
+  for (let i = 0; i < report.length - 1; i++) {
+    if (!isPairSafe(report[i], report[i + 1], isAsc)) {
+      safe = false;
+      break;
+    }
+  }
+  return safe;
 }
