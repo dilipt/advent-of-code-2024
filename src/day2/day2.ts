d5090feda3761e260342bef1956c54d9dd261c2c
Dilip Thomas <dthomas84@gmail.com>
2024-12-03 22:22:22 +1100

    still failing day 2.

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -36,12 +36,13 @@ export function part2(filename: string) {
     .split("\n")
     .map((line) => line.split(" ").map((ch) => parseInt(ch)));
 
-  const unsafeReports = [];
+  const unsafeReports: { [key: number]: number[][] } = {};
   let safeCount = 0;
-  for (let report of reports) {
+
+  for (let j = 0; j < reports.length; j++) {
+    let report = reports[j];
     if (report[0] === report[1]) {
-      // TODO: push both slice(1) and splice(1, 1) and test both.
-      unsafeReports.push(report.slice(1));
+      unsafeReports[j] = [report.slice(1)];
       continue;
     }
 
@@ -50,17 +51,21 @@ export function part2(filename: string) {
     for (let i = 0; i < report.length - 1; i++) {
       const isCurrentPairAsc = report[i] < report[i + 1];
       if ((isAsc && !isCurrentPairAsc) || (!isAsc && isCurrentPairAsc)) {
-        // TODO: push both splice(i, 1) and splice(i + 1, 1) and test both.
-        report.splice(i, 1);
-        unsafeReports.push(report);
+        const unsafe1 = [...report];
+        const unsafe2 = [...report];
+        unsafe1.splice(i, 1);
+        unsafe2.splice(i + 1, 1);
+        unsafeReports[j] = [unsafe1, unsafe2];
         safe = false;
         break;
       }
       const diff = Math.abs(report[i] - report[i + 1]);
       if (diff < 1 || diff > 3) {
-        // TODO: push both splice(i, 1) and splice(i + 1, 1) and test both.
-        report.splice(i + 1, 1);
-        unsafeReports.push(report);
+        const unsafe1 = [...report];
+        const unsafe2 = [...report];
+        unsafe1.splice(i, 1);
+        unsafe2.splice(i + 1, 1);
+        unsafeReports[j] = [unsafe1, unsafe2];
         safe = false;
         break;
       }
@@ -71,26 +76,36 @@ export function part2(filename: string) {
     }
   }
 
-  return unsafeReports.reduce((count, report) => {
-    if (report[0] === report[1]) {
-      return count;
-    }
-
-    const isAsc = report[0] < report[1];
-    for (let i = 0; i < report.length - 1; i++) {
-      const currentPairIsAsc = report[i] < report[i + 1];
-      if ((isAsc && !currentPairIsAsc) || (!isAsc && currentPairIsAsc)) {
-        console.log(`unsafe: ${JSON.stringify(report)}`);
-        return count;
+  for (const [key, unsafes] of Object.entries(unsafeReports)) {
+    for (const report of unsafes) {
+      if (report[0] === report[1]) {
+        continue;
       }
-      const diff = Math.abs(report[i] - report[i + 1]);
-      if (diff < 1 || diff > 3) {
-        console.log(`unsafe: ${JSON.stringify(report)}`);
-        return count;
+
+      let safe = true;
+      const isAsc = report[0] < report[1];
+      for (let i = 0; i < report.length - 1; i++) {
+        const currentPairIsAsc = report[i] < report[i + 1];
+        if ((isAsc && !currentPairIsAsc) || (!isAsc && currentPairIsAsc)) {
+          console.log(`${key} unsafe: ${JSON.stringify(report)}`);
+          safe = false;
+          break;
+        }
+        const diff = Math.abs(report[i] - report[i + 1]);
+        if (diff < 1 || diff > 3) {
+          console.log(`${key} unsafe: ${JSON.stringify(report)}`);
+          safe = false;
+          break;
+        }
+      }
+
+      if (safe) {
+        console.log(`${key} safe! ${JSON.stringify(report)}`);
+        safeCount++;
+        break;
       }
     }
+  }
 
-    console.log(`safe! ${JSON.stringify(report)}`);
-    return count + 1;
-  }, safeCount);
+  return safeCount;
 }
